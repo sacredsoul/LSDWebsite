5af0d0c46e6a88e999f5982ecbff5922812f2113
SacreDsouL <lsd2323@163.com>
2020-01-23 19:09:47 +0800

    init

---
+++ www/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="app">
+        <Home></Home>
+    </div>
+</template>
+
+<script>
+    import Home from './components/Home.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            Home
+        }
+    }
+
+</script>
+
+<style>
+    #app {
+        /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: white;
+    }
+
+    body {
+        font-family: "Poppins", Arial, sans-serif;
+        background: #121212;
+        font-size: 18px;
+        line-height: 1.8;
+        font-weight: 400;
+        position: relative;
+    }
+
+    body:after {
+        position: fixed;
+        top: -150px;
+        right: -50px;
+        bottom: 0;
+        content: '';
+        width: 400px;
+        height: 600px;
+        border: 2px solid rgba(255, 255, 255, 0.05);
+        z-index: -1;
+        -webkit-transform: rotate(-30deg);
+        -moz-transform: rotate(-30deg);
+        -ms-transform: rotate(-30deg);
+        -o-transform: rotate(-30deg);
+        transform: rotate(-30deg);
+    }
+    body:before {
+        position: fixed;
+        top: 500px;
+        left: -100px;
+        bottom: 0;
+        content: '';
+        width: 400px;
+        height: 400px;
+        border: 2px solid rgba(255, 255, 255, 0.05);
+        z-index: -1;
+        -webkit-transform: rotate(20deg);
+        -moz-transform: rotate(20deg);
+        -ms-transform: rotate(20deg);
+        -o-transform: rotate(20deg);
+        transform: rotate(20deg);
+    }
+</style>
---
+++ www/src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="home">
+        <img alt="Alpha logo" src="../assets/alpha_logo.png">
+        <h1 class="main-title">{{text}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        data() {
+            return {
+                text: "Hi, I' m LSD"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .home {
+        font-size: 28px;
+    }
+    .home img {
+        margin-top: 60px;
+        width: 200px;
+        height: 200px;
+    }
+
+    .main-title {
+        padding-top: 60px;
+    }
+</style>
